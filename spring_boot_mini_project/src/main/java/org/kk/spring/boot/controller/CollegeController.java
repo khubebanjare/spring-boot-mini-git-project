d234fab0d752b257e3a5370cb092c9ed22634046
khube <khube@khube>
2021-01-18 22:40:38 +0530

    khube:added new api in CollegeController

--- spring_boot_mini_project/src/main/java/org/kk/spring/boot/controller/CollegeController.java
+++ spring_boot_mini_project/src/main/java/org/kk/spring/boot/controller/CollegeController.java
@@ -1,6 +1,14 @@
 package org.kk.spring.boot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/college")
 public class CollegeController{
 
-
+	@RequestMapping ("/message")
+	public String newMessage() {
+		return "Welcome to Spring Boot Git Project to GitHub";
+	}
 }
